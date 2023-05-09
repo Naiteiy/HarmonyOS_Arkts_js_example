92a7139beb7cb3408f80ac55f4480d977776ab15
Naiteiy <1820031511@qq.com>
2023-05-09 17:47:17 +0800

    修改

--- entry/src/main/js/MainAbility/pages/start/start.js
+++ entry/src/main/js/MainAbility/pages/start/start.js
@@ -1,8 +1,49 @@
+import router from '@ohos.router';
+import document from '@ohos.document';
+import { Button } from '@ohos.multimodalInput.mouseEvent';
 export default {
+
     data: {
-        title: ""
+        clickImage: "/common/img/index_fill.png",
+        clickImage1: "/common/img/my.png",
+        isPressed: true,
+        isPressed1: false
     },
-    onInit() {
-        this.title = "Hello World";
+
+    ifclick() {
+
+        if (this.isPressed = true) {
+            this.clickImage = "/common/img/index_fill.png";
+            this.clickImage1 = '/common//img/my.png';
+            this.isPressed = true;
+            this.isPressed1 = false;
+            this.$element('button1').setStyle('display','flex');
+            this.$element('button2').setStyle('display','flex');
+            this.$element('button3').setStyle('display','flex');
+            this.$element('button4').setStyle('display','flex');
+            this.$element('my').setStyle('display','none');
+        }
+
+    },
+    ifclick1() {
+
+        if (this.isPressed = true) {
+
+            this.clickImage1 = "/common/img/my_fill.png";
+            this.clickImage = '/common//img/index.png';
+            this.isPressed1 = true;
+            this.isPressed = false;
+            this.$element('button1').setStyle('display','none');
+            this.$element('button2').setStyle('display','none');
+            this.$element('button3').setStyle('display','none');
+            this.$element('button4').setStyle('display','none');
+            this.$element('my').setStyle('display','flex');
+        }
+
+
+    },
+    logout(){
+        router.push({url:"pages/index/index"});
     }
+
 }
